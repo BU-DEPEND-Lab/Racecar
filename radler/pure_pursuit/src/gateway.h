5c13333f0838a6bf40cbabeac241c2b1fc24e729
QifanHe <heqifan0109@gmail.com>
2017-09-11 22:18:17 -0400

    radler update

--- radler/pure_pursuit/src/gateway.h
+++ radler/pure_pursuit/src/gateway.h
@@ -1,14 +1,15 @@
 #include RADL_HEADER
-
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Point.h"
 using namespace std;
 
 class Gateway {
 public:
 	Gateway() {
 		sub = h.subscribe("slam_out_pose", 1, &Gateway::subhandler, this);
+		pub = h.advertise<geometry_msgs::Point>("drive_parameters", 1);
 	}
 
 	void subhandler(const geometry_msgs::PoseStamped::ConstPtr& msg) {
@@ -26,6 +27,14 @@ public:
   			o->slam_out_pose->orientation_y = this->out_posestamped->pose.orientation.y;
   			o->slam_out_pose->orientation_z = this->out_posestamped->pose.orientation.z;
   			o->slam_out_pose->orientation_w = this->out_posestamped->pose.orientation.w;
+  			
+  			// Forward Radler msg to ROS
+  			geometry_msgs::Point out_msgs;
+  			out_msgs.x = i->drive_parameters->velocity;
+  			out_msgs.y = i->drive_parameters->angle;
+  			this->pub.publish(out_msgs);
+
+
   		} else {
       radl_turn_on(radl_STALE, &o_f->slam_out_pose);
     	}
@@ -33,6 +42,7 @@ public:
 private:
 	ros::NodeHandle h;
 	ros::Subscriber sub;
+	ros::Publisher pub;
 	geometry_msgs::PoseStamped::ConstPtr out_posestamped;
 
 };
